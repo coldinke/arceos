ee0ed3e151a60bb159ba93af8ca593db9c122c1f
coldinke <yangzhibo.yang@gmail.com>
2023-11-15 16:31:52 +0800

    Add the test of HashMap

--- apps/memtest/src/main.rs
+++ apps/memtest/src/main.rs
@@ -7,6 +7,8 @@ extern crate axstd as std;
 
 use rand::{rngs::SmallRng, RngCore, SeedableRng};
 use std::collections::BTreeMap;
+// use std::collections::HashMap;
+use axstd::map::HashMap;
 use std::vec::Vec;
 
 fn test_vec(rng: &mut impl RngCore) {
@@ -38,6 +40,22 @@ fn test_btree_map(rng: &mut impl RngCore) {
     println!("test_btree_map() OK!");
 }
 
+fn test_hashmap_map(rng: &mut impl RngCore) {
+    const N: usize = 50_000;
+    let mut m = HashMap::new();
+    for _ in 0..N {
+        let value = rng.next_u32();
+        let key = format!("key_{value}");
+        m.insert(key, value);
+    }
+    for (k, v) in m.iter() {
+        if let Some(k) = k.strip_prefix("key_") {
+            assert_eq!(k.parse::<u32>().unwrap(), *v);
+        }
+    }
+    println!("tset_hashmap_map() Ok!");
+}
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     println!("Running memory tests...");
@@ -45,6 +63,7 @@ fn main() {
     let mut rng = SmallRng::seed_from_u64(0xdead_beef);
     test_vec(&mut rng);
     test_btree_map(&mut rng);
+    test_hashmap_map(&mut rng);
 
     println!("Memory tests run OK!");
 }

fd6cd5d5f4523ec078b885a4e8aead75ef09a2ea
coldinke <yangzhibo.yang@gmail.com>
2023-11-27 16:11:42 +0800

    exercise 6

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -9,7 +9,7 @@ use axstd::println;
 use axhal::misc::terminate;
 
 const PLASH_START: usize = 0x22_000_000;
-const RUN_START: usize = 0xffff_ffc0_8010_0000;
+const RUN_START: usize = 0x4010_0000;
 
 const SYS_HELLO: usize = 1;
 const SYS_PUTCHAR: usize = 2;
@@ -45,7 +45,6 @@ fn main() {
         }
     );
     offset += 8;
-
     // register the sbi instruction?
     // Add the number of SYSCALL to handle function
     register_abi(SYS_HELLO, abi_hello as usize);
@@ -53,16 +52,19 @@ fn main() {
     register_abi(SYS_TERMINATE, abi_terminate as usize);
 
     println!("Loading payload...");
-    for _i in 0..apps_num {
+    for i in 0..apps_num {
+        unsafe { init_app_page_table(); }
+        unsafe { switch_app_aspace(); }
+
         let app = APPInfo::load(plash_start, offset);
-        // println!("App: {}, size: {}, load code: {:?}, address :[{:?}]", i, app.app_size, app.app_data, app.app_data.as_ptr());
+        println!("App: {}, size: {}, address :[{:?}]", i, app.app_size, app.app_data.as_ptr());
         offset = offset + 8 + (app.app_size as isize);
 
         let run_code = unsafe {
             core::slice::from_raw_parts_mut(RUN_START as *mut u8, app.app_size)
         };
         run_code.copy_from_slice(app.app_data);
-        // println!("run code: {:?}, address [{:?}]", run_code, run_code.as_ptr());
+        println!("start address [{:?}]", run_code.as_ptr());
 
         unsafe { core::arch::asm!("
             la      a0, {abi_table}
@@ -73,7 +75,7 @@ fn main() {
             clobber_abi("C")
         )}
 
-        // // clear
+        // clear
         unsafe {
             ptr::write_bytes(run_code.as_mut_ptr(), 0, run_code.len())
         };
@@ -107,4 +109,21 @@ impl APPInfo {
             app_data,
         }
     }
+}
+
+// Application space
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+unsafe fn init_app_page_table() {
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize - axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
 }
